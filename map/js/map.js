2971cb48a1486a58e3aa038a7d009b0725decbcb
CT-HAI <junyan0803@gmail.com>
2017-07-29 12:04:17 +0800

    Update map.js

--- map/js/map.js
+++ map/js/map.js
@@ -1,7 +1,25 @@
-document.write('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>');
+document.write('<script type="text/javascript" src="../jquery-3.1.1/jquery-3.1.1.min.js"></script>');
 var scale = 5;//初始化缩放比例
 var isClick = 0;//判断用户是否点击地图上的元素
 var coordX_c = null;//初始化被点击元素的横坐标,作为该元素被点击的标识符
+var browser = {  
+        versions: function (obj) {
+            var u = navigator.userAgent, app = navigator.appVersion;
+            return {         //移动终端浏览器版本信息
+                trident搜索: u.indexOf('Trident') > -1, //IE内核
+                presto: u.indexOf('Presto') > -1, //opera内核
+                webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+                mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+                iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+                iPad: u.indexOf('iPad') > -1, //是否iPad
+                webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+            };
+        }(),
+        language:(navigator.browserLanguage || navigator.language).toLowerCase()
+    }
 function getCoord(event){
 	var Abscissa  = event.offsetX;
 	var Ordinate = event.offsetY;
